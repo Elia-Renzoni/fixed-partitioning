7f2bf62e5f64aa878518e704ddfdfed31525cce1
Elia Renzoni <elia.renzoni03@gmail.com>
2026-02-25 15:59:09 +0100

    chore: add level creation logics
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- internal/store/memtable.go
+++ internal/store/memtable.go
@@ -12,21 +12,46 @@ type MemTable struct {
 }
 
 type skipListNode struct {
-	key      []byte
-	value    []byte
-	weight   int
-	nextNode *skipListNode
+	key        []byte
+	value      []byte
+	selfLevel  int
+	nodeLevels []*skipListNode
+	nextNode   *skipListNode
+}
+
+func newSkipListNode(key, value []byte) *skipListNode {
+	return &skipListNode{
+		key:        key,
+		value:      value,
+		nodeLevels: make([]*skipListNode, 0, 16),
+	}
+}
+
+func (s *skipListNode) selfCopy(latestLevel int) {
+	var l int
+	if latestLevel < len(s.nodeLevels) {
+		l = latestLevel + 1
+	}
+	s.nodeLevels = append(s.nodeLevels, &skipListNode{
+		key:        s.key,
+		value:      s.value,
+		selfLevel:  l,
+		nodeLevels: s.nodeLevels,
+	})
 }
 
 func NewMemTable() MemTable {
 	return MemTable{}
 }
 
-func (m *MemTable) AddDocument() {
+func (m *MemTable) AddDocument(documentKey, documentBody []byte) error {
+	return nil
 }
 
-func (m *MemTable) DeleteDocument() {
+func (m *MemTable) DeleteDocument(documentKey []byte) error {
+	return nil
 }
 
-func (m *MemTable) FetchDocument() {
+func (m *MemTable) FetchDocument(documentKey []byte) error {
+	return nil
 }
