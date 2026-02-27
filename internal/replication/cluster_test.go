65bed1bfaef10a4a2ea8ba63850f57f887628810
Elia Renzoni <elia.renzoni03@gmail.com>
2026-02-27 11:36:46 +0100

    chore: add test
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

---
+++ internal/replication/cluster_test.go
@@ -0,0 +1,14 @@
+package replication_test
+
+import (
+	"testing"
+)
+
+func TestCluster(t *testing.T) {
+	nodes := []string{
+		"172.89.34.23:6060",
+		"172.123.32.216:5050",
+		"172.44.11.22.1:4040",
+	}
+
+}
