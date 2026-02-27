b22ab6fe9b045052dba4b1eab51bd290e3f98eb0
Elia Renzoni <elia.renzoni03@gmail.com>
2026-02-27 17:41:13 +0100

    fix: failing test
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

--- internal/replication/cluster_test.go
+++ internal/replication/cluster_test.go
@@ -10,7 +10,7 @@ func TestCluster(t *testing.T) {
 	nodes := []string{
 		"172.89.34.23:6060",
 		"172.123.32.216:5050",
-		"172.44.11.22.1:4040",
+		"172.44.11.22:4040",
 	}
 
 	cluster := replication.NewCluster()
@@ -18,5 +18,17 @@ func TestCluster(t *testing.T) {
 		if err := cluster.AddNode(node); err != nil {
 			t.Fatal(err)
 		}
+	}
+
+	if cluster.GetNodeFromLocation(0) != "172.89.34.23:6060" {
+		t.Fatal(0)
+	}
+
+	if cluster.GetNodeFromLocation(1) != "172.123.32.216:5050" {
+		t.Fatal(1)
+	}
+
+	if cluster.GetNodeFromLocation(2) != "172.44.11.22:4040" {
+		t.Fatal(2)
 	}
 }
