1bd05bc2516716636f9eac0100c5974eed772ae5
Elia Renzoni <118525453+Elia-Renzoni@users.noreply.github.com>
2026-03-07 15:26:08 +0100

    feat: add partition table manager (#6)
    * feat: add partition table manager
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>
    
    * chore: add partition manipulation
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>
    
    * chore: add partition merger
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>
    
    * chore: add partition balancer
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>
    
    * refactor: update partition table
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>
    
    * chore: add test and reduce duplicate nodes per partitions
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>
    
    * refactor: update test
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>
    
    * fix: failing test while merging partitions
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>
    
    * fix: delete redunant node assignement
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>
    
    * fix: avoid duplicated nodes and unbalanced lists
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>
    
    * refactor: commented unuseful code section
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>
    
    ---------
    
    Signed-off-by: Elia Renzoni <elia.renzoni03@gmail.com>

---
+++ internal/sharding/partition_manager.go
@@ -0,0 +1,167 @@
+package sharding
+
+import (
+	"errors"
+	"fmt"
+	"hash"
+	"hash/fnv"
+	"maps"
+	"math/rand/v2"
+	"slices"
+	"sync"
+
+	"github.com/fixed-partitioning/internal/replication"
+)
+
+type PartitionTable struct {
+	hashSlots         int
+	clusterLen        int
+	cluster           *replication.Cluster
+	pTable            map[int][]string
+	hasher            hash.Hash64
+	averageSlots      int
+	mutex             sync.RWMutex
+	replicationFactor int
+}
+
+const minClusterLen int = 4
+
+func NewPartitionTable(slots, rp int, cluster *replication.Cluster) *PartitionTable {
+	return &PartitionTable{
+		hashSlots:         slots,
+		pTable:            make(map[int][]string),
+		cluster:           cluster,
+		hasher:            fnv.New64(),
+		replicationFactor: rp,
+	}
+}
+
+// only the coordinator can call this method
+func (p *PartitionTable) AssignPartitions() error {
+	if p.cluster.Len() < minClusterLen {
+		return errors.New("unable to assign partition due to lack of nodes")
+	}
+
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	var errs = p.hashSlots
+
+	for slot := range p.hashSlots {
+		partitionIndex := slot % p.cluster.Len()
+		attachedNode := p.cluster.GetNodeFromLocation(partitionIndex)
+		if attachedNode != "" && !p.nodeAlreadyPresent(p.pTable[partitionIndex], attachedNode) {
+			p.pTable[partitionIndex] = append(p.pTable[partitionIndex], attachedNode)
+			rfNodes := p.completeNodesWithRF(attachedNode)
+			if rfNodes != nil {
+				copy(p.pTable[partitionIndex], rfNodes)
+			}
+		}
+		errs--
+	}
+
+	// compute average slots
+	p.averageSlots = p.hashSlots / p.cluster.Len()
+
+	if errs > 0 {
+		return fmt.Errorf("assigned only %d partitions", errs)
+	}
+	return nil
+}
+
+func (p *PartitionTable) nodeAlreadyPresent(nodes []string, targetNode string) bool {
+	return slices.Contains(nodes, targetNode)
+}
+
+func (p *PartitionTable) completeNodesWithRF(attachedNode string) []string {
+	if p.replicationFactor > 0 {
+		var (
+			nodes         = make([]string, 0)
+			sprintCounter int
+		)
+
+		for {
+			nodeID := rand.IntN(p.cluster.Len())
+			selectedNode := p.cluster.GetNodeFromLocation(nodeID)
+			if selectedNode == attachedNode || slices.Contains(nodes, selectedNode) {
+				continue
+			}
+
+			sprintCounter += 1
+			nodes = append(nodes, selectedNode)
+			if sprintCounter >= p.replicationFactor {
+				break
+			}
+		}
+		return nodes
+	}
+	return nil
+}
+
+func (p *PartitionTable) ReadPartitionTable() map[int][]string {
+	p.mutex.RLock()
+	defer p.mutex.RUnlock()
+
+	return p.pTable
+}
+
+/*
+type delta struct {
+	nodeAddress string
+
+	// diff contains the effective delta between
+	// the ideal partitions per node and the real
+	// partition per node.
+	// example: Ideal -> 10 per node
+	//          Real ->  15 per node
+	//          Diff ->  -5
+	// example: Ideal -> 10 per node
+	//          Real -> 3 per node
+	//          Diff -> 7
+	diff int
+}
+
+func (p *PartitionTable) BalancePartitions() {
+	p.mutex.RLock()
+	defer p.mutex.RUnlock()
+
+	var unbalancedNodes = make([]delta, 0)
+
+	// search the unbalanced nodes
+	for _, pNodes := range p.pTable {
+		for _, node := range pNodes {
+			counter := 0
+			for _, assignedNodes := range p.pTable {
+				if slices.Contains(assignedNodes, node) {
+					counter += 1
+				}
+			}
+
+			if (counter - 1) != p.averageSlots {
+				unbalancedNodes = append(unbalancedNodes, delta{
+					nodeAddress: node,
+					diff:        p.averageSlots - counter,
+				})
+			}
+		}
+	}
+
+	if !(len(unbalancedNodes) > 0) {
+		return
+	}
+
+	// TODO-> implements the balance logic
+}*/
+
+func (p *PartitionTable) MergePartitions(table map[int][]string) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	// in case of the first ever request
+	if len(p.pTable) == 0 {
+		p.pTable = table
+		return
+	}
+
+	maps.Copy(p.pTable, table)
+}
---
+++ internal/sharding/partition_manager_test.go
@@ -0,0 +1,53 @@
+package sharding_test
+
+import (
+	"fmt"
+	"slices"
+	"testing"
+
+	"github.com/fixed-partitioning/internal/replication"
+	"github.com/fixed-partitioning/internal/sharding"
+)
+
+func TestPartitionTable(t *testing.T) {
+	members := replication.NewCluster()
+
+	// forcing error generation
+	ptable := sharding.NewPartitionTable(500, 0, members)
+	if err := ptable.AssignPartitions(); err == nil {
+		t.Fatal()
+	}
+
+	dec := 6
+	helper := 0
+	for i := range 30 {
+		helper += 1
+		if i == 10 || i == 20 {
+			dec += 1
+			helper = 0
+		}
+		address := fmt.Sprintf("127.0.0.1:60%d%d", dec, helper)
+		members.AddNode(address)
+	}
+	ptable = sharding.NewPartitionTable(500, 3, members)
+	err := ptable.AssignPartitions()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for p, nodes := range ptable.ReadPartitionTable() {
+		t.Logf("partition: %d - nodes: %s", p, nodes)
+	}
+
+	newSlots := map[int][]string{
+		500: []string{"127.0.0.1:6060"},
+	}
+
+	ptable.MergePartitions(newSlots)
+
+	table := ptable.ReadPartitionTable()
+	nodes := table[500]
+	if !slices.Contains(nodes, "127.0.0.1:6060") {
+		t.Fatalf("got: %s", nodes)
+	}
+}
